f85ece1912063196f1e8a8db8da7059f519dd2d2
Gabriel Torres Gamez <gabriel.torres@fuet.ch>
2023-11-09 14:41:35 +0000

    train with optimized logs

--- src/models/ResNet50BinaryClassifier.py
+++ src/models/ResNet50BinaryClassifier.py
@@ -38,8 +38,11 @@ class ResNet50BinaryClassifier(pl.LightningModule):
 
 
 class ResNet50BinaryClassifier(pl.LightningModule):
-    def __init__(self):
+    def __init__(self, lr=1e-3, weight_decay=1e-4):
         super().__init__()
+        self.lr = lr
+        self.weight_decay = weight_decay
+        
         self.resnet50 = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
         num_features = self.resnet50.fc.in_features
         self.resnet50.fc = nn.Sequential(nn.Linear(num_features, 1), nn.Sigmoid())
@@ -84,45 +87,22 @@ class ResNet50BinaryClassifier(pl.LightningModule):
         self.train_labels.append(binary_labels)
         self.train_preds.append(outputs)
 
-        self.log("train_loss", loss)
+        self.log("train_loss", loss, prog_bar=True, logger=True, batch_size=len(batch))
         return loss
     
     def on_train_epoch_end(self):
         train_labels = torch.cat(self.train_labels, dim=0)
         train_preds = torch.cat(self.train_preds, dim=0)
 
-        precision = self.precision(train_preds, train_labels)
-        recall = self.recall(train_preds, train_labels)
+        train_precision = self.precision(train_preds, train_labels)
+        train_recall = self.recall(train_preds, train_labels)
 
-        self.log("train_precision", precision)
-        self.log("train_recall", recall)
+        self.log("train_precision", train_precision, prog_bar=True, logger=True)
+        self.log("train_recall", train_recall, prog_bar=True, logger=True)
 
         self.train_labels = []
-        self.train_preds = []  
-
-    def test_step(self, batch, batch_idx):
-        outputs, binary_labels = self.__step(batch)
-        loss = nn.BCELoss()(outputs, binary_labels)
-
-        self.test_labels.append(binary_labels)
-        self.test_preds.append(outputs)
-
-        self.log("test_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
-        return loss
-
-    def on_test_epoch_end(self):
-        test_labels = torch.cat(self.test_labels, dim=0)
-        test_preds = torch.cat(self.test_preds, dim=0)
-
-        precision = self.precision(test_preds, test_labels)
-        recall = self.recall(test_preds, test_labels)
-
-        self.log("test_precision", precision)
-        self.log("test_recall", recall)
-
-        self.test_labels = []
-        self.test_preds = []
-
+        self.train_preds = [] 
+    
     def validation_step(self, batch, batch_idx):
         outputs, binary_labels = self.__step(batch)
         loss = nn.BCELoss()(outputs, binary_labels)
@@ -131,21 +111,44 @@ class ResNet50BinaryClassifier(pl.LightningModule):
         self.val_labels.append(binary_labels.int())
         self.val_preds.append(predictions)
 
-        self.log("val_loss", loss)
+        self.log("val_loss", loss, prog_bar=True, logger=True, batch_size=len(batch))
         return loss
-
+    
     def on_validation_epoch_end(self):
         val_labels = torch.cat(self.val_labels, dim=0)
         val_preds = torch.cat(self.val_preds, dim=0)
 
-        precision = self.precision(val_preds, val_labels)
-        recall = self.recall(val_preds, val_labels)
+        val_precision = self.precision(val_preds, val_labels)
+        val_recall = self.recall(val_preds, val_labels)
 
-        self.log("val_precision", precision)
-        self.log("val_recall", recall)
+        self.log("val_precision", val_precision, prog_bar=True, logger=True)
+        self.log("val_recall", val_recall, prog_bar=True, logger=True)
 
         self.val_labels = []
         self.val_preds = []
 
+    def test_step(self, batch, batch_idx):
+        outputs, binary_labels = self.__step(batch)
+        loss = nn.BCELoss()(outputs, binary_labels)
+
+        self.test_labels.append(binary_labels)
+        self.test_preds.append(outputs)
+
+        self.log("test_loss", loss, prog_bar=True, logger=True, batch_size=len(batch))
+        return loss
+
+    def on_test_epoch_end(self):
+        test_labels = torch.cat(self.test_labels, dim=0)
+        test_preds = torch.cat(self.test_preds, dim=0)
+
+        test_precision = self.precision(test_preds, test_labels)
+        test_recall = self.recall(test_preds, test_labels)
+
+        self.log("test_precision", test_precision, prog_bar=True, logger=True)
+        self.log("test_recall", test_recall, prog_bar=True, logger=True)
+
+        self.test_labels = []
+        self.test_preds = []
+
     def configure_optimizers(self):
-        return optim.Adam(self.parameters(), lr=1e-3, weight_decay=1e-4)
+        return optim.Adam(self.parameters(), lr=self.lr, weight_decay=self.weight_decay)
