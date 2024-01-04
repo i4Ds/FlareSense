7a0ed8d81d68e9597ab81fd9cdf173b6a200e80f
Gabriel Torres Gamez <gabriel.torres@fuet.ch>
2024-01-04 09:22:06 +0100

    run resnet50 on 15min data

--- slurm/run_nb.sh
+++ slurm/run_nb.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
-#SBATCH --time=16:00:00
+#SBATCH --time=72:00:00
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=16G
 #SBATCH --gres=gpu:1
-#SBATCH --partition=top6
+#SBATCH --partition=performance
 #SBATCH --out=slurm/log_out.txt
 #SBATCH --err=slurm/log_err.txt
 #SBATCH --job-name="FlareSense"
+#SBATCH --exclude=gpu22a,gpu22b,node15,sdas2
 python3 -m pip install -r requirements.txt
 echo
 
-dvc pull
+#dvc pull
 cd notebooks
-python3 -m papermill 00-Unzip.ipynb 00-Unzip.ipynb -k 'python3'
-echo
+#python3 -m papermill 00-Unzip.ipynb 00-Unzip.ipynb -k 'python3'
+#echo
 
-NB_PATH="06-ResNet18.ipynb"
+NB_PATH="10-ResNet50.ipynb"
 python3 -m papermill $NB_PATH $NB_PATH -k 'python3'
 
 echo "Done!"
